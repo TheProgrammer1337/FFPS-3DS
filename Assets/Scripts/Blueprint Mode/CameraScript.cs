8715eaf4d7fa513313e3f278419490faa78d814c
TheProgrammer1337 <TheGamer3212@ProtonMail.com>
2024-09-05 16:54:21 -0500

    Improved Blueprint Mode movement

--- Assets/Scripts/Blueprint Mode/CameraScript.cs
+++ Assets/Scripts/Blueprint Mode/CameraScript.cs
@@ -2,89 +2,31 @@
 
 public class CameraScript : MonoBehaviour
 {
-    public float moveSpeed = 5;
-
-
-    void Start()
-    {
-
-    }
-
+    public float moveSpeed = 5f;
+    public Vector2 xBounds = new Vector2(-67.6033f, 52.6033f);
+    public Vector2 yBounds = new Vector2(-33.8871f, 27.7546f);
 
     void Update()
     {
+        float moveX = UnityEngine.N3DS.GamePad.CirclePad.x * moveSpeed * Time.deltaTime;
+        float moveY = UnityEngine.N3DS.GamePad.CirclePad.y * moveSpeed * Time.deltaTime;
 
-        if (Input.GetKey(KeyCode.RightArrow))
-        {
-            if (transform.position.x > -67.6033)
-            {
-                transform.position += Vector3.right * -moveSpeed * Time.deltaTime;
+        Vector3 newPosition = transform.position - new Vector3(moveX, moveY, 0);
 
-            }
-        }
+        // Clamp the position within the specified bounds
+        newPosition.x = Mathf.Clamp(newPosition.x, xBounds.x, xBounds.y);
+        newPosition.y = Mathf.Clamp(newPosition.y, yBounds.x, yBounds.y);
 
-        if (Input.GetKey(KeyCode.Keypad6))
-        {
-            if (transform.position.x > -67.6033)
-            {
-                transform.position += Vector3.right * -moveSpeed * Time.deltaTime;
-
-            }
-        }
-
-        if (Input.GetKey(KeyCode.LeftArrow))
-        {
-            if (transform.position.x < 52.6033)
-            {
-                transform.position += Vector3.right * moveSpeed * Time.deltaTime;
-            }
-        }
-
-        if (Input.GetKey(KeyCode.Keypad4))
-        {
-            if (transform.position.x < 52.6033)
-            {
-                transform.position += Vector3.right * moveSpeed * Time.deltaTime;
-            }
-        }
-
-        if (Input.GetKey(KeyCode.UpArrow))
-        {
-            if (transform.position.y > -33.8871)
-            {
-                transform.position += Vector3.up * -moveSpeed * Time.deltaTime;
-
-            }
-
-        }
-
-        if (Input.GetKey(KeyCode.Keypad8))
-        {
-            if (transform.position.y > -33.8871)
-            {
-                transform.position += Vector3.up * -moveSpeed * Time.deltaTime;
-
-            }
-
-        }
-
-        if (Input.GetKey(KeyCode.DownArrow))
-        {
-            if (transform.position.y < 27.7546)
-            {
-                transform.position += Vector3.up * moveSpeed * Time.deltaTime;
-
-            }
-
-        }
-        if (Input.GetKey(KeyCode.Keypad2))
-        {
-            if (transform.position.y < 27.7546)
-            {
-                transform.position += Vector3.up * moveSpeed * Time.deltaTime;
-
-            }
-
-        }
+        transform.position = newPosition;
+#if UNITY_EDITOR
+        if (Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.Keypad6))
+            transform.position += Vector3.left * moveSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.Keypad4))
+            transform.position += Vector3.right * moveSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.Keypad8))
+            transform.position += Vector3.down * moveSpeed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.Keypad2))
+            transform.position += Vector3.up * moveSpeed * Time.deltaTime;
+#endif
     }
 }
