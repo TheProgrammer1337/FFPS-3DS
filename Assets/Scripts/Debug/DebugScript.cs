e3505f97bf8c4acf95544d6ab43f76762c7da8c4
TheProgrammer1337 <TheGamer3212@ProtonMail.com>
2024-09-23 15:29:16 -0500

    Organize project files

---
+++ Assets/Scripts/Debug/DebugScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DebugScript : MonoBehaviour
+{
+	void OnGUI()
+	{
+		Rect rect = new Rect(90, 10, 200, 30);
+		GUI.Label(rect, "   Sys : " + UnityEngine.N3DS.Debug.GetSystemFree());
+
+		rect.y += 40;
+		GUI.Label(rect, "Device : " + UnityEngine.N3DS.Debug.GetDeviceFree());
+
+		rect.y += 40;
+		GUI.Label(rect, "VRAM A : " + UnityEngine.N3DS.Debug.GetVRAMAFree());
+
+		rect.y += 40;
+		GUI.Label(rect, "VRAM B : " + UnityEngine.N3DS.Debug.GetVRAMBFree());
+
+
+	}
+}
